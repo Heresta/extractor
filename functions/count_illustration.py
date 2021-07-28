aafc87a99353898a94cf7dbcaa5cf8075e438ec0
Heresta <claire.jahan@gmail.com>
2021-07-28 12:10:21 +0200

    change ident

--- functions/count_illustration.py
+++ functions/count_illustration.py
@@ -8,22 +8,22 @@ from lxml import etree as ET
         :return: str
             Gives how many pages, decorations, drop capitals and figures there are in document
         """
-    document_final = ET.parse(doc)
-    racine = document_final.getroot()
-    page = 0
-    decoration = 0
-    dropcapital = 0
-    figure = 0
-    for surfacegrp in racine[1]:
-        page +=1
-        for surface in surfacegrp:
-            if surface.get('type') == 'decoration':
-                decoration += 1
-            elif surface.get('type') == 'dropcapital':
-                dropcapital += 1
-            elif surface.get('type') == 'figure':
-                figure += 1
-    print("page : " + str(page))
-    print("decoration : " + str(decoration))
-    print("dropcapital : " + str(dropcapital))
-    print("figure : " + str(figure))
+   	 document_final = ET.parse(doc)
+   	 racine = document_final.getroot()
+   	 page = 0
+   	 decoration = 0
+   	 dropcapital = 0
+   	 figure = 0
+   	 for surfacegrp in racine[1]:
+       		 page +=1
+       		 for surface in surfacegrp:
+           		 if surface.get('type') == 'decoration':
+               			 decoration += 1
+           		 elif surface.get('type') == 'dropcapital':
+               			 dropcapital += 1
+           		 elif surface.get('type') == 'figure':
+               			 figure += 1
+   	 print("page : " + str(page))
+   	 print("decoration : " + str(decoration))
+   	 print("dropcapital : " + str(dropcapital))
+   	 print("figure : " + str(figure))
